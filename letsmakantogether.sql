5c6775110d882bd5c23981a7ea94193de903c08f
legitaxes <pohjiaqi1@gmail.com>
2023-10-16 16:07:53 +0800

    fixed and corrected database script

--- letsmakantogether.sql
+++ letsmakantogether.sql
@@ -12,9 +12,136 @@ Use LetsMakanTogether
 GO
 
 
+/***************************************************************/
+/***           Delete tables before creating                 ***/
+-- Delete in order:
+-- location
+-- fooddetail
+-- groupmeeting
+-- meeting
+-- groupregionpreference
+-- groupfoodpreference
+-- grouppicture
+-- groupmember
+-- groupchatmessages
+-- groupchat
+-- group
+-- userregionpreference
+-- userfoodpreference
+-- userpicture
+-- regionpreference
+-- foodpreference
+-- user
+/***************************************************************/
+/* Table: Location */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.Location') and sysstat & 0xf = 3)
+  drop table dbo.Location
+GO
+
+/* Table: FoodDetail */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.FoodDetail') and sysstat & 0xf = 3)
+  drop table dbo.FoodDetail
+GO
+
+/* Table: GroupMeeting */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.GroupMeeting') and sysstat & 0xf = 3)
+  drop table dbo.GroupMeeting
+GO
+
+/* Table: Meeting */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.Meeting') and sysstat & 0xf = 3)
+  drop table dbo.Meeting
+GO
+
+/* Table: GroupRegionPreference */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.GroupRegionPreference') and sysstat & 0xf = 3)
+  drop table dbo.GroupRegionPreference
+GO
+
+/* Table: GroupFoodPreference */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.GroupFoodPreference') and sysstat & 0xf = 3)
+  drop table dbo.GroupFoodPreference
+GO
+
+/* Table: GroupPicture */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.GroupPicture') and sysstat & 0xf = 3)
+  drop table dbo.GroupPicture
+GO
+
+/* Table: GroupMember */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.GroupMember') and sysstat & 0xf = 3)
+  drop table dbo.GroupMember
+GO
+
+/* Table: GroupChatMessages */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.GroupChatMessages') and sysstat & 0xf = 3)
+  drop table dbo.GroupChatMessages
+GO
+
+/* Table: GroupChat */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.GroupChat') and sysstat & 0xf = 3)
+  drop table dbo.GroupChat
+GO
+
+/* Table: Group */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.[Group]') and sysstat & 0xf = 3)
+  drop table dbo.[Group]
+GO
+
+/* Table: UserRegionPreference */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.UserRegionPreference') and sysstat & 0xf = 3)
+  drop table dbo.UserRegionPreference
+GO
+
+/* Table: UserFoodPreference */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.UserFoodPreference') and sysstat & 0xf = 3)
+  drop table dbo.UserFoodPreference
+GO
+
+/* Table: UserPicture */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.UserPicture') and sysstat & 0xf = 3)
+  drop table dbo.UserPicture
+GO
+
+/* Table: RegionPreference */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.RegionPreference') and sysstat & 0xf = 3)
+  drop table dbo.RegionPreference
+GO
+
+/* Table: FoodPreference */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.FoodPreference') and sysstat & 0xf = 3)
+  drop table dbo.FoodPreference
+GO
+
+/* Table: User */
+if exists(Select * from sysobjects
+  where id = object_id('dbo.[User]') and sysstat & 0xf = 3)
+  drop table dbo.[User]
+GO
+
+
+/***************************************************************/
+/***                     Creating tables                     ***/
+/***************************************************************/
 /* User Components */ 
 /* Table: dbo.User */
-CREATE TABLE dbo.User
+CREATE TABLE dbo.[User]
 (
   [UserID] 			              int                     IDENTITY (1,1),
   [UserName]			            varchar(50)		          NOT NULL,
@@ -49,7 +176,7 @@ CREATE TABLE dbo.RegionPreference
   [RegionPreferenceID]        int                   IDENTITY (1,1),
   [RegionType]                varchar(255)          NOT NULL,
 
-  CONSTRAINT PK_Preference PRIMARY KEY NONCLUSTERED (RegionPreferenceID)
+  CONSTRAINT PK_Region_Preference PRIMARY KEY NONCLUSTERED (RegionPreferenceID)
 )
 GO
 
@@ -63,7 +190,7 @@ CREATE TABLE dbo.UserPicture
 
   CONSTRAINT PK_UserPicture PRIMARY KEY NONCLUSTERED (PictureID),
   CONSTRAINT FK_UserPicture_UserID FOREIGN KEY (UserID)
-  REFERENCES dbo.User(UserID)
+  REFERENCES dbo.[User](UserID)
 )
 GO
 
@@ -75,12 +202,12 @@ CREATE TABLE dbo.UserFoodPreference -- contains food preferences linked to the u
   [UserID]                    int                   NOT NULL,
   [FoodPreferenceID]          int                   NOT NULL,
 
-  CONSTRAINT PK_UserPreference PRIMARY KEY NONCLUSTERED (UserFoodPreferenceID),
-  CONSTRAINT FK_UserPreference_UserID FOREIGN KEY (UserID)
-  REFERENCES dbo.User(UserID),
+  CONSTRAINT PK_User_Food_Preference PRIMARY KEY NONCLUSTERED (UserFoodPreferenceID),
+  CONSTRAINT FK_User_Food_Preference_UserID FOREIGN KEY (UserID)
+  REFERENCES dbo.[User](UserID),
   CONSTRAINT FK_UserPreference_FoodPreferenceID FOREIGN KEY (FoodPreferenceID)
   REFERENCES dbo.FoodPreference(FoodPreferenceID),
-  CONSTRAINT UQ_UserPreference UNIQUE (UserID, FoodPreferenceID)
+  CONSTRAINT UQ_User_Food_Preference UNIQUE (UserID, FoodPreferenceID)
 )
 GO
 
@@ -92,19 +219,19 @@ CREATE TABLE dbo.UserRegionPreference -- contains region preferences linked to t
   [UserID]                    int                  NOT NULL,
   [RegionPreferenceID]        int                  NOT NULL,
 
-  CONSTRAINT PK_UserPreference PRIMARY KEY NONCLUSTERED (UserRegionPreferenceID),
-  CONSTRAINT FK_UserPreference_UserID FOREIGN KEY (UserID)
-  REFERENCES dbo.User(UserID),
+  CONSTRAINT PK_User_Region_Preference PRIMARY KEY NONCLUSTERED (UserRegionPreferenceID),
+  CONSTRAINT FK_User_Region_Preference_UserID FOREIGN KEY (UserID)
+  REFERENCES dbo.[User](UserID),
   CONSTRAINT FK_UserPreference_RegionPreferenceID FOREIGN KEY (RegionPreferenceID)
   REFERENCES dbo.RegionPreference(RegionPreferenceID),
-  CONSTRAINT UQ_UserPreference UNIQUE (UserID, RegionPreferenceID)
+  CONSTRAINT UQ_User_Region_Preference UNIQUE (UserID, RegionPreferenceID)
 )
 GO
 
 
 /* Group Components */
 /* Table: Group */
-CREATE TABLE dbo.Group
+CREATE TABLE dbo.[Group]
 (
   [GroupID]                   int                   IDENTITY (1,1), 
   [GroupName]                 varchar(50)           NOT NULL,
@@ -126,9 +253,9 @@ CREATE TABLE dbo.GroupChat
 
   CONSTRAINT PK_GroupChat PRIMARY KEY NONCLUSTERED (GroupChatID),
   CONSTRAINT FK_GroupChat_GroupID FOREIGN KEY (GroupID)
-  REFERENCES dbo.Group(GroupID),
+  REFERENCES dbo.[Group](GroupID),
   CONSTRAINT FK_GroupChat_UserID FOREIGN KEY (UserID)
-  REFERENCES dbo.User(UserID)
+  REFERENCES dbo.[User](UserID)
 )
 GO
 
@@ -147,7 +274,7 @@ CREATE TABLE dbo.GroupChatMessages
   CONSTRAINT FK_GroupChatMessages_GroupChatID FOREIGN KEY (GroupChatID)
   REFERENCES dbo.GroupChat(GroupChatID),
   CONSTRAINT FK_GroupChatMessages_UserID FOREIGN KEY (UserID)
-  REFERENCES dbo.User(UserID)
+  REFERENCES dbo.[User](UserID)
 )
 GO
 
@@ -161,9 +288,9 @@ CREATE TABLE dbo.GroupMember
   
   CONSTRAINT PK_GroupMember PRIMARY KEY NONCLUSTERED (GroupMemberID),
   CONSTRAINT FK_GroupMember_UserID FOREIGN KEY (UserID)
-  REFERENCES dbo.User(UserID),
+  REFERENCES dbo.[User](UserID),
   CONSTRAINT FK_GroupMember_GroupID FOREIGN KEY (GroupID)
-  REFERENCES dbo.Group(GroupID)
+  REFERENCES dbo.[Group](GroupID)
 )
 GO
 
@@ -177,7 +304,7 @@ CREATE TABLE dbo.GroupPicture
 
   CONSTRAINT PK_GroupPicture PRIMARY KEY NONCLUSTERED (PictureID),
   CONSTRAINT FK_GroupPicture_GroupID FOREIGN KEY (GroupID)
-  REFERENCES dbo.Group(GroupID)
+  REFERENCES dbo.[Group](GroupID)
 )
 GO
 
@@ -189,12 +316,12 @@ CREATE TABLE dbo.GroupFoodPreference -- contains food preferences linked to the 
   [GroupID]                   int                   NOT NULL, -- FK to Group
   [FoodPreferenceID]          int                   NOT NULL, -- FK to FoodPreference
 
-  CONSTRAINT PK_GroupPreference PRIMARY KEY NONCLUSTERED (GroupFoodPreferenceID),
-  CONSTRAINT FK_GroupPreference_GroupID FOREIGN KEY (GroupID)
-  REFERENCES dbo.Group(GroupID),
+  CONSTRAINT PK_Group_Food_Preference PRIMARY KEY NONCLUSTERED (GroupFoodPreferenceID),
+  CONSTRAINT FK_Group_Food_Preference_GroupID FOREIGN KEY (GroupID)
+  REFERENCES dbo.[Group](GroupID),
   CONSTRAINT FK_GroupPreference_FoodPreferenceID FOREIGN KEY (FoodPreferenceID)
   REFERENCES dbo.FoodPreference(FoodPreferenceID),
-  CONSTRAINT UQ_GroupPreference UNIQUE (GroupID, FoodPreferenceID)
+  CONSTRAINT UQ_Group_Food_Preference UNIQUE (GroupID, FoodPreferenceID)
 )
 GO
 
@@ -206,12 +333,12 @@ CREATE TABLE dbo.GroupRegionPreference -- contains region preferences linked to 
   [GroupID]                  int                    NOT NULL, -- FK to Group
   [RegionPreferenceID]       int                    NOT NULL, -- FK to RegionPreference
 
-  CONSTRAINT PK_GroupPreference PRIMARY KEY NONCLUSTERED (GroupRegionPreferenceID),
-  CONSTRAINT FK_GroupPreference_GroupID FOREIGN KEY (GroupID)
-  REFERENCES dbo.Group(GroupID),
+  CONSTRAINT PK_Group_Region_Preference PRIMARY KEY NONCLUSTERED (GroupRegionPreferenceID),
+  CONSTRAINT FK_Group_Region_Preference_GroupID FOREIGN KEY (GroupID)
+  REFERENCES dbo.[Group](GroupID),
   CONSTRAINT FK_GroupPreference_RegionPreferenceID FOREIGN KEY (RegionPreferenceID)
   REFERENCES dbo.RegionPreference(RegionPreferenceID),
-  CONSTRAINT UQ_GroupPreference UNIQUE (GroupID, RegionPreferenceID)
+  CONSTRAINT UQ_Group_Region_Preference UNIQUE (GroupID, RegionPreferenceID)
 )
 GO
 
@@ -226,7 +353,7 @@ CREATE TABLE dbo.Meeting
 
   CONSTRAINT PK_Meeting PRIMARY KEY NONCLUSTERED (MeetingID),
   CONSTRAINT FK_Meeting_GroupID FOREIGN KEY (GroupID)
-  REFERENCES dbo.Group(GroupID)
+  REFERENCES dbo.[Group](GroupID)
 )
 GO
 
@@ -240,7 +367,7 @@ CREATE TABLE dbo.GroupMeeting
 
   CONSTRAINT PK_GroupMeeting PRIMARY KEY NONCLUSTERED (GroupMeetingID),
   CONSTRAINT FK_GroupMeeting_GroupID FOREIGN KEY (GroupID)
-  REFERENCES dbo.Group(GroupID),
+  REFERENCES dbo.[Group](GroupID),
   CONSTRAINT FK_GroupMeeting_MeetingID FOREIGN KEY (MeetingID)
   REFERENCES dbo.Meeting(MeetingID)
 )
@@ -287,6 +414,6 @@ GO
 -- data and entries are CASE SENSITIVE!!
 SET IDENTITY_INSERT [dbo].[User] ON 
 INSERT [dbo].[User] ([UserID], [UserName], [Name], [Email], [Age], [Gender], [Password]) VALUES (1, 'Sanrio123', 'Cinnamon', 'sanrio_cinnamon@gmail.com', 18, 'F', '123456')
-INSERT [dbo].[User] ([UserID], [UserName], [Name], [Email], [Age], [Gender], [Password]) VALUES (1, 'Tanny23', 'Tom Tan', 'tantom@gmail.com', 24, 'M', 'ilovepeanuts')
+INSERT [dbo].[User] ([UserID], [UserName], [Name], [Email], [Age], [Gender], [Password]) VALUES (2, 'Tanny23', 'Tom Tan', 'tantom@gmail.com', 24, 'M', 'ilovepeanuts')
 SET IDENTITY_INSERT [dbo].[User] OFF
 
