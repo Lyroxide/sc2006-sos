efa3f9b063297e66efd803203e3ab60c446429f3
Lyroxide <frezlefedora@gmail.com>
2023-10-30 23:41:41 +0900

    login and register up

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -2,9 +2,7 @@ export default function authHeader() {
     let user = JSON.parse(localStorage.getItem('user'));
 
     if (user && user.accessToken) {
-        return { Authorization: 'Bearer ' + user.accessToken };
-        // for Node.js Express back-end
-        // return { 'x-access-token': user.accessToken };
+        return { 'x-access-token': user.accessToken };
     } else {
         return {};
     }
