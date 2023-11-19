0fa12bbb3f1a97e8b3d9cbfdb8657727eb58481e
kevintanyh <95677318+kevintanyh@users.noreply.github.com>
2023-11-19 18:43:04 +0800

    Delete deprecatedscripts directory

--- deprecatedscripts/copyMysql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-# Copy All the Relevant Database File
-rm -rf ./docker/mysql/letsmakantogether_mysql.sql
-cp -rf ./database/* ./docker/mysql/
--- deprecatedscripts/copyNginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# Copy all the relevant frontend files (Vue & Nginx)
-rm -rf ./docker/nginx/vue
-cp -rf vue ./docker/nginx/
-cp -rf nginx/* ./docker/nginx/
